50f3d745f098afc14790035f9a80ca5cfe4e0d6c
derrickma <51876299+derrickmaa@users.noreply.github.com>
2024-04-28 17:34:58 +0900

    Create MessageFormat.py

---
+++ MessageFormat.py
@@ -0,0 +1,31 @@
+class MessageFormat:
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
+
+    def encode_header(self, message):
+        tokens = []
+        tokens.extend(self.tokenizer.convert_tokens_to_ids(self.tokenizer.tokenize("<|start_header_id|>")))
+        print(tokens)
+        tokens.extend(self.tokenizer.convert_tokens_to_ids(self.tokenizer.tokenize(message["role"])))
+        tokens.extend(self.tokenizer.convert_tokens_to_ids(self.tokenizer.tokenize("<|end_header_id|>")))
+        tokens.extend(self.tokenizer.convert_tokens_to_ids(self.tokenizer.tokenize("\n\n")))
+        return tokens
+
+    def encode_message(self, message):
+        tokens = self.encode_header(message)
+        tokens.extend(
+            self.tokenizer.convert_tokens_to_ids(self.tokenizer.tokenize(message["content"].strip()))
+        )
+        tokens.extend(self.tokenizer.convert_tokens_to_ids(self.tokenizer.tokenize("<|eot_id|>")))
+        tokens.extend(self.tokenizer.convert_tokens_to_ids(self.tokenizer.tokenize("\n\n")))
+        return tokens
+
+    def encode_dialog_prompt(self, dialog):
+        tokens = []
+        tokens.extend(
+            self.tokenizer.convert_tokens_to_ids(self.tokenizer.tokenize("<|begin_of_text|>")))
+        for message in dialog:
+            tokens.extend(self.encode_message(message))
+        # Add the start of an assistant message for the model to complete.
+        tokens.extend(self.encode_header({"role": "assistant", "content": ""}))
+        return tokens
